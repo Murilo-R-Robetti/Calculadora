7f176660c64a4aaa58bc4b467d2a47bb048cfcfb
Murilo R R <130069044+Murilo-R-Robetti@users.noreply.github.com>
2024-12-05 12:00:05 -0300

    adsbkbjkvjabcbkjcacasnjcsajn

---
+++ src/calculadora.test.ts
@@ -0,0 +1,17 @@
+
+import { Sinal } from "./calculadora"
+import { Controle } from "./calculadora"
+import { Operação } from "./calculadora"
+import { Digito } from "./calculadora"
+import CpuA1 from "./cpuA1"
+import TelaX0Teste from "./telaX0Teste"
+
+describe("Testando minha calculadora", () => {
+  
+})
+    test("Testar soma 12+34+56", ()=>{
+        
+    })
+    test("Testar soma 12/10", ()=>{
+        
+    })
